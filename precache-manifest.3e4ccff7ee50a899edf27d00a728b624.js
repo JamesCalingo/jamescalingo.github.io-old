2ef84ac1e92cadbe077c5e0b630c60580a6adf8f
JC <james.calingo@gmail.com>
2020-08-14 13:06:23 -0400

    Updates

--- precache-manifest.3e4ccff7ee50a899edf27d00a728b624.js
+++ precache-manifest.3e4ccff7ee50a899edf27d00a728b624.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb3d47c7c19421dd94a43a8303aae1d4",
+    "revision": "d24b0157149a82359a1eba79f101e2b8",
     "url": "/index.html"
   },
   {
-    "revision": "2cbaa6cacc20449d18f1",
+    "revision": "5a62cf84b2b0cdb5f808",
     "url": "/static/css/main.8570f4ed.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b481e48b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2cbaa6cacc20449d18f1",
-    "url": "/static/js/main.05b735e2.chunk.js"
+    "revision": "5a62cf84b2b0cdb5f808",
+    "url": "/static/js/main.6080479f.chunk.js"
   },
   {
     "revision": "ca361c02b51ee89a2ead",
