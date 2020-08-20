f83255d8f3afe457369cae03e3e36d94e18c007d
JC <james.calingo@gmail.com>
2020-08-20 17:58:40 -0400

    deleted unused code

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -43,32 +43,4 @@ class Portfolio extends React.Component {
   }
 }
 
-
-// function Portfolio() {
-//   return (
-//     <div className="container">
-//       <div className="card">
-//         <div className="card-header"><h1>A few of the (awesome) things I've worked on</h1></div>
-//         <div className="card-body">
-//           <div className="row">
-//             <div className="col-12 col-md-4">
-//             {/* Cards for projects */}
-//                 <PortfolioCard key={projects.id} title={projects.title} />
-
-
-//               {/* END CARD */}
-//             </div>
-//           </div>
-//           <div className="row text-center">
-//           <div className="col-12">
-//           <h1 className="portfolioTag">MORE TO COME SOON</h1>
-//           </div>
-//           </div>
-//         </div>
-//       </div>
-
-//     </div>
-//   )
-// }
-
 export default Portfolio
