8b60da3b7d34e5250a340c997afbec749d4cb0b5
JC <james.calingo@gmail.com>
2021-05-06 15:56:30 -0400

    updating projects

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import React from 'react'
-import { HashRouter, Route } from 'react-router-dom';
-import Landing from "./components/pages/Landing"
-import About from './components/pages/About'
-import Technologies from './components/pages/Technologies/Technologies'
-import Portfolio from './components/pages/Portfolio/Portfolio'
-import Contact from './components/pages/Contact'
-import Sidebar from './components/Sidebar'
-import MobileMenu from "./components/pages/MobileMenu"
-import './App.css'
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Landing from "./components/pages/Landing";
+import About from "./components/pages/About";
+import Technologies from "./components/pages/Technologies/Technologies";
+import Portfolio from "./components/pages/Portfolio/Portfolio";
+import Contact from "./components/pages/Contact";
+import Sidebar from "./components/Sidebar";
+import MobileMenu from "./components/pages/MobileMenu";
+import "./App.css";
 
-
-function App () {
+function App() {
   return (
-   
     <div className="container-grid">
       <Sidebar />
-    
+
       <div className="pages">
-      <HashRouter basename="/">
-        <Route exact path="/" component={Landing} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/technologies" component={Technologies} />
-      <Route exact path="/portfolio" component={Portfolio} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/mobile-menu" component={MobileMenu} />
-      </HashRouter>
-      <footer id="copyright">
-    © James Calingo
-    </footer>
+        <HashRouter basename="/">
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/technologies" component={Technologies} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/mobile-menu" component={MobileMenu} />
+        </HashRouter>
+        <footer id="copyright">© James Calingo</footer>
       </div>
-    
- 
-
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
